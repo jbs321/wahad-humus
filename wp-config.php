bbf4d9d2d7074e44cba7fb5c407c26bc7bea0a8f
Jacob <jbs321.work@gmail.com>
2016-05-31 00:48:37 -0700

    add configurations

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wahad_humus');
+define('DB_NAME', 'wahad-humus');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Aa123456');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
