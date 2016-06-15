d9f6eda6fc1d051f3151188a7ea91653a43092e4
Jacob <jbs321.work@gmail.com>
2016-06-15 13:22:35 -0700

    add copy post pluign

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wahad-humus');
+define('DB_NAME', 'wahad_humus');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Aa123456');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
